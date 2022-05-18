4b27993a66f8dbba58b7963f67dd026a1c1a2628
PanzeriTommaso <panzeri.tommasoambro@studenti.salesianisesto.it>
2022-05-18 09:30:56 +0200

    control exist of value

--- es3.js
+++ es3.js
@@ -1,33 +1,39 @@
     var count=3;
-    $("#btn1").click(function(){
-        var n1= +$("#N1").val();
-        var n2= +$("#N2").val();
-        var res=n1+n2;
-        console.log (res);
-        $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>+</td><td>"+n2+"</td><td>"+res+"</td></tr>");
-        count ++;
-    });
-    $("#btn2").click(function(){
-        var n1=$("#N1").val();
-        var n2=$("#N2").val();
-        var res=n1-n2;
-        console.log (res);
-        $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>-</td><td>"+n2+"</td><td>"+res+"</td></tr>");
-        count ++;
-    });
-    $("#btn3").click(function(){
-        var n1=$("#N1").val();
-        var n2=$("#N2").val();
-        var res=n1*n2;
-        console.log (res);
-        $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>*</td><td>"+n2+"</td><td>"+res+"</td></tr>");
-        count ++;
-    });
-    $("#btn4").click(function(){
-        var n1=$("#N1").val();
-        var n2=$("#N2").val();
-        var res=n1/n2;
-        console.log (res);
-        $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>/</td><td>"+n2+"</td><td>"+res+"</td></tr>");
-        count ++;
-    });
+    $("#btn1").click(function(){calcola("+");});
+    $("#btn2").click(function(){calcola("-");});
+    $("#btn3").click(function(){calcola("*");});
+    $("#btn4").click(function(){calcola("/");});
+
+    function calcola(segno){
+
+        if ($("#N1").val() && $("#N2").val()){
+            var n1= +$("#N1").val();
+            var n2= +$("#N2").val();
+            count++;
+            switch(segno) {
+                case "+":
+                    var res=n1+n2;
+                    console.log (res);
+                    $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>+</td><td>"+n2+"</td><td>"+res+"</td></tr>");ì
+                break;
+                case "-":
+                    var res=n1-n2;
+                    console.log (res);
+                    $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>-</td><td>"+n2+"</td><td>"+res+"</td></tr>");
+                break;
+                case "*":
+                    var res=n1*n2;
+                    console.log (res);
+                    $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>*</td><td>"+n2+"</td><td>"+res+"</td></tr>");
+                break;
+                case "/":
+                    var res=n1/n2;
+                    console.log (res);
+                    $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>/</td><td>"+n2+"</td><td>"+res+"</td></tr>");
+                break;
+            }
+        }else{
+            alert("COMPLETA TUTTI I CAMPI");
+            
+        }
+    }
